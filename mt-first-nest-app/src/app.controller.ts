70c649b3b764f5267c0f9611b0f878912511eb90
IseAkira <akira.ise.m@gmail.com>
2026-07-30 16:40:33 +0900

    nest

--- mt-first-nest-app/src/app.controller.ts
+++ mt-first-nest-app/src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  POST,
+  PUT,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +17,49 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  findAll(): string {
+    return '全ユーザーを取得';
+  }
+
+  @Get('profile')
+  getProfile(): string {
+    return 'ユーザープロフィールを取得';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `ID: ${id}のユーザーを取得`;
+  }
+
+  @PUT(':id')
+  update(@Param('id') id: string): string {
+    return `ID: ${id} のユーザーを更新`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string): string {
+    return `ID: ${id} のユーザーを削除`;
+  }
+
+  @Post()
+  create(): string {
+    return '新しいユーザーを作成';
+  }
+}
+
+@Controller('products')
+export class ProductsController {
+  @Get()
+  findAll(
+    @Query('limit') limit: string,
+    @Query('offset') offset: string,
+  ): string {
+    return `limit:${limit},offset:${offset}の商品を取得`;
   }
 }
